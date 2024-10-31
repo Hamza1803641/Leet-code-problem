45207090cf3beb73c9431709d192f3fd21db1ac2
Hamza Mehmood <mehmood.hamza777@gmail.com>
2024-11-01 04:10:03 +0500

    Update find _duplicate_number.cpp

--- find _duplicate_number.cpp
+++ find _duplicate_number.cpp
@@ -2,3 +2,13 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        while (nums[0] != nums[nums[0]]) {
+            swap(nums[0], nums[nums[0]]);
+        }
+        return nums[0];
+    }
+};
